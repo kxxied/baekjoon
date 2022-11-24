1160ab76666ab163418789d997b81f6c77652d47
kxxied <117827546+kxxied@users.noreply.github.com>
2022-11-24 14:45:55 +0900

    Delete 입출력과 사칙연산 directory

--- 입출력과 사칙연산/1008.py
+++
@@ -1,6 +0,0 @@
-while True:
-     A, B = map(int, input().split())
-     if 0 < A and B < 10:
-         break
-
-print(A / B)
--- 입출력과 사칙연산/18108.py
+++
@@ -1,6 +0,0 @@
-while True:
-     y = int(input())
-     if 1000 <= y and y <= 3000:
-         break
-
-print(y-543)
